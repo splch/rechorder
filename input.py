57d56504a9272bea7aa92de9bc08afd887f9c2ba
spencerchurchill <spencer.churchill@outlook.com>
2019-12-22 10:57:08 -0800

    conversion equation update

--- input.py
+++ input.py
@@ -9,7 +9,6 @@ music = []
 
 class rechord(object):
     def __init__(self):
-
         # stream constants
         self.CHUNK = 1024 * 2
         self.FORMAT = pyaudio.paInt16
@@ -18,6 +17,7 @@ class rechord(object):
         self.pause = False
 
         self.notes = []
+        self.hz = None
 
         # stream object
         self.p = pyaudio.PyAudio()
@@ -36,23 +36,24 @@ class rechord(object):
         fs_max = -np.sort(-self.fs)
 
         self.keys = []
-        for f in fs_max[1:4]:
-            hz = (np.where(self.fs == f)[0][0]) * self.RATE / self.CHUNK # convert FFT to hz
+        i = 0
+        while not self.hz:
+            i += 1
+            hz = (np.where(self.fs == fs_max[i])[0][0]) * self.RATE / self.CHUNK # convert FFT to hz
             key = int(round(12 * np.log2(hz/440) + 49)) # hz to note on piano
 
             if key > 0 and key < 109:
-                self.keys.append(key)
-    
+                self.key = key
+                self.hz = round(hz)
+
     def display(self):
+        note = notes[(self.key - 1) % 12] + str((self.key+8) // 12) # note as string
+        music.append(note)
 
-        if self.keys:
-            note = notes[(self.keys[0] - 1) % 12] + str(self.keys[0]//12)
-            music.append(note)
-
-            print(note)
+        print(note, '\t', self.hz)
+        self.hz = None
 
     def mic_input(self):
-
         print("stream started\n")
 
         try:
@@ -64,7 +65,7 @@ class rechord(object):
                 if np.percentile(np.abs(data_np), 90) >= 200: # check volume if a note is being played
                     # compute FFT and update line
                     yf = fft(data_int)
-                    self.fs = np.abs(yf[0:self.CHUNK]) / (128 * self.CHUNK)
+                    self.fs = np.abs(yf[0:self.CHUNK])
 
                     self.conv()
                     self.display()
