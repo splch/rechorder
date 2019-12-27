580a0944f15d38534c878a97211820c229c09c25
spencerchurchill <spencer.churchill@outlook.com>
2019-12-26 18:39:15 -0800

    file updates

--- Rechord - optimized.py
+++ Rechord - optimized.py
@@ -4,53 +4,15 @@ import struct
 from scipy.fftpack import fft
 import music21 # install musescore
 
-def conv(fs):
-    fs_max = -np.sort(-fs)
-
-    key = None
-    i = 0
-    while not key:
-        i += 1
-        hz = round((np.where(fs == fs_max[i])[0][0]) * RATE / CHUNK) # convert FFT to hz
-        note = int(round(12 * np.log2(hz/440) + 49)) # hz to note on piano
-
-        if note > 0 and note < 109:
-            key = note
-    
-    return key, hz
-
-def display(key, hz):
+def display(key, hz, notes):
     note = scale[(key - 1) % 12] + str((key+8) // 12) # note as string
     notes.append(note)
 
     print(note, '\t', hz)
 
-def record():
-    print("stream started\n")
-
-    try:
-        while True:
-            data = stream.read(CHUNK)
-            data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
-            data_np = np.array(data_int, dtype='b')[::2] + 128
-
-            if np.percentile(np.abs(data_np), 90) >= 200: # check volume if a note is being played
-                # compute FFT and update line
-                fs = np.abs(fft(data_int)[0:CHUNK])
-
-                key, hz = conv(fs)
-                display(key, hz)
-            else:
-                notes.append('r')
-                # continue
-
-    except KeyboardInterrupt:
-        p.close(stream)
-        print("\n\nstream ended\n\n")
-
 def transcribe(notes): # optimize length algorithm
     for i, note in enumerate(notes):
-        try:
+        if len(note) > 1:
             octave = int(note[-1])
 
             if octave == 0:
@@ -71,7 +33,7 @@ def transcribe(notes): # optimize length algorithm
                 notes[i] = note[:-1].lower() + "'" * 3
             elif octave == 8:
                 notes[i] = note[:-1].lower() + "'" * 4
-        except:
+        else:
             continue
 
     music = []
@@ -91,10 +53,52 @@ def transcribe(notes): # optimize length algorithm
             i = j
             i += 1
 
+    # octave: 0, CCCC | 1, CCC | 2, CC | 3, C | 4, c| 5, c' | 6, c'' | 7, c''' | 8, c'''' |
     # print("music21.converter.parse(\"tinynotation: 4/4 " + " ".join(music) + "\").show()")
-
     music21.converter.parse("tinynotation: 4/4 " + " ".join(music)).show()
-    # octave: 0, CCCC | 1, CCC | 2, CC | 3, C | 4, c| 5, c' | 6, c'' | 7, c''' | 8, c'''' | 
+    return 0
+
+def conv(fs):
+    fs_max = -np.sort(-fs)
+
+    key = None
+    i = 0
+    while not key:
+        i += 1
+        hz = round((np.where(fs == fs_max[i])[0][0]) * RATE / CHUNK) # convert FFT to hz
+        note = int(round(12 * np.log2(hz/440) + 49)) # hz to note on piano
+
+        if note > 0 and note < 109:
+            key = note
+    
+    return key, hz
+
+def record(rests = True):
+    notes = []
+    
+    print("stream started\n")
+
+    try:
+        while True:
+            data = stream.read(CHUNK, exception_on_overflow=False)
+            data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+            data_np = np.array(data_int, dtype='b')[::2] + 128
+
+            if np.percentile(np.abs(data_np), 90) >= 200: # check volume if a note is being played
+                # compute FFT and update line
+                fs = np.abs(fft(data_int)[0:CHUNK])
+
+                key, hz = conv(fs)
+                display(key, hz, notes)
+            elif rests and len(notes) > 1:
+                notes.append('r')
+                # continue
+
+    except KeyboardInterrupt:
+        p.close(stream)
+        print("\n\nstream ended\n\n")
+    
+    return notes
 
 # stream constants
 CHUNK = 1024 * 2
@@ -115,8 +119,6 @@ stream = p.open(
 
 scale = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"]
 
-notes = []
+notes = record(rests=False) # interrupt to end stream
 
-if __name__ == "__main__":
-    record()
-    transcribe(notes)
+transcribe(notes)
