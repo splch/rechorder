fb1de3cf5607c622ef7640f8b4d4269241a7fb40
Spencer Churchill <spencer.l.churchill@gmail.com>
2021-07-30 12:29:22 -0700

    create library 0.1

---
+++ README.md
@@ -0,0 +1,6 @@
+# ReChord  — Convert Mic to Music
+## Puts the us in Mic :wink: jk sry
+
+## Requirements
+Install [musescore](https://musescore.org/en/handbook/3/installation).
+Run `pip install -r requirements.txt` in your development environment.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rechorder',  
+    version='0.1',
+    scripts=['rechorder'] ,
+    author="Spencer Churchill",
+    author_email="spencer.l.churchill@gmail.com",
+    description="A microphone to sheet music converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/splch/rechorder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
